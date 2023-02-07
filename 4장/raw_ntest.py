4ee97618a71010c9e532a580f36fd2715e32d56e
SuyeonSim <124683716+SuyeonSim@users.noreply.github.com>
2023-02-07 21:27:24 +0900

    Delete 4장 directory

--- 4장/raw_ntest.py
+++
@@ -1,9 +0,0 @@
-
-height = 5
-
-for i in range(1, height + 1):
-    result = ""
-    result += "*" * (height - i)
-    result += " " * (2 * i - 1)
-    result += "*" * (height - i)
-    print(result)
--- 4장/studenttest.py
+++
@@ -1,9 +0,0 @@
-
-def f():
-    x=10
-    y=20
-    return x, y
-
-(a, b) = f()
-
-print(a)
